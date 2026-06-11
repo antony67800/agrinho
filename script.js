44167cfbc3c71bcd1f4e0bf021c892532b1766aa
antony67800 <antony.pedroso@escola.pr.gov.br>
2026-06-11 16:02:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const botao = document.getElementById('btn-leia-mais');
+const detalhes = document.getElementById('mais-detalhes');
+
+botao.addEventListener('click', () => {
+    const aberto = detalhes.classList.toggle('aberto');
+    botao.textContent = aberto ? 'Mostrar menos' : 'O QUE É?';
+});
+
+const botaoEquilibrio = document.getElementById('btn-equilibrio');
+const comoAlcancar = document.getElementById('como-alcancar');
+
+botaoEquilibrio.addEventListener('click', () => {
+    const aberto = comoAlcancar.classList.toggle('aberto');
+    botaoEquilibrio.textContent =
+        aberto ? 'Mostrar menos' : 'COMO ALCANÇAR?';
+});
+
+const botaoExemplos = document.getElementById('btn-exemplos');
+const exemplos = document.getElementById('exemplos');
+
+botaoExemplos.addEventListener('click', () => {
+    const aberto = exemplos.classList.toggle('aberto');
+    botaoExemplos.textContent =
+        aberto ? 'Mostrar menos' : 'EXEMPLOS';
+});
+
+function irParaConclusao() {
+    document.getElementById('conclusao').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+const modal = document.getElementById("imagem-ampliada");
+const modalImg = document.getElementById("img-ampliada");
+const imagens = document.querySelectorAll(".carrossel img");
+const fechar = document.querySelector(".fechar");
+
+imagens.forEach(img => {
+    img.addEventListener("click", () => {
+        modal.style.display = "block";
+        modalImg.src = img.src;
+        modalImg.alt = img.alt;
+    });
+});
+
+fechar.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+});
